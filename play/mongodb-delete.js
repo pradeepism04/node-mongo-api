cdab4b7cc233b4a74e45cecfee3da02feb15695a
Your Name <you@example.com>
2019-01-31 13:34:14 +0530

    add delete code

---
+++ play/mongodb-delete.js
@@ -0,0 +1,29 @@
+const {MongoClient, ObjectID} = require ('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client)=>{
+	if(err){
+		console.log('Unable to connect mongodb server');
+	}
+	console.log('Connected to MongoDB Server');
+	const db = client.db('TodoApp');
+
+	// deletemany
+		// db.collection('Todos').deleteMany({text: 'eatlunch'}).then((result)=>{
+		// 	console.log(result);
+		// })
+	//deleteOne 
+		// db.collection('Todos').deleteOne({text:'eatlunch'}).then(( result)=>{
+		// 	console.log(result);
+		// });
+
+	// findeOneAnddelete
+	// db.collection.find({text:'eatlunch'}).deleteOne({text:'eatlunch'}).then((result)=>{
+	// 	console.log(result);
+	// });
+	// OR 
+	// db.collection('Todos').findOneAndDelete({text: 'eatlunch'}).then((result)=>{
+	// 	console.log(result);
+	// });
+
+	client.close();
+});
