d28a92d57a4662232a46886143c96f9db1362856
Your Name <you@example.com>
2019-01-31 19:11:06 +0530

    get the todos data

--- server/server.js
+++ server/server.js
@@ -29,6 +29,15 @@ app.post('/todos', (req, res)=>{
 
 
 
+app.get('/todos', (req, res)=>{
+	Todo.find().then((todos)=>{
+		res.send({
+			todos
+		})
+	}, (err)=>{
+		res.status(400).send(err);
+	});
+});
 
 
 
